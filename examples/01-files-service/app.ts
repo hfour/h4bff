26ab676c08f9d58e348bb2dadc4fe13fcc519e9c
Gorgi Kosev <gk@hfour.com>
2018-09-22 23:55:24 +0100

    improve testability, prepare for tests

--- examples/01-files-service/app.ts
+++ examples/01-files-service/app.ts
@@ -1,17 +1,17 @@
 import * as express from 'express';
 import { App, ContextualRouter } from 'h4b2'; // from h4b2
 
-import { FilesPlugin, FilePermissions } from './files-plugin';
-import { Database } from './database';
+import { FilesPlugin } from './files-plugin';
+//import { Database } from './database';
 
 export default class MyApp extends App {
   constructor() {
     super();
     this.load(FilesPlugin);
-    this.getSingleton(FilePermissions);
   }
 
   start() {
+    this.activate();
     const expressApp = express();
     this.getSingleton(ContextualRouter).install('/', expressApp);
     console.log('listening on http://localhost:8080/');
@@ -19,7 +19,7 @@ export default class MyApp extends App {
   }
 
   migrate() {
-    const bla = this.getSingleton(Database).getMigrationsList();
+    //const bla = this.getSingleton(Database).runMigrations();
   }
 }
 
