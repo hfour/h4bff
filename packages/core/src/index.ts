20772d4f3da31cf108da4782c2a2114236a9f842
Goran Peshanski <gpeshans@hfour.com>
2019-01-22 12:27:12 +0100

    add support for nested apps + example

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -5,7 +5,11 @@ export type ConstructorOrFactory<U, T> = ClassFactory<U, T> | ClassConstructor<U
 export type PublicInterface<T> = { [K in keyof T]: T[K] };
 
 export class App {
-  private singletonLocator = new Locator(this, s => '__appSingleton' in s);
+  private singletonLocator: Locator<this>;
+
+  constructor(singletonLocator?: Locator<any>) {
+    this.singletonLocator = singletonLocator || new Locator(this, s => '__appSingleton' in s);
+  }
 
   /**
    * @internal
@@ -37,6 +41,11 @@ export class App {
 
   createServiceContext() {
     return new ServiceContext(this);
+  }
+
+  inheritWithSingletonContext() {
+    let singletonLocator = new Locator(this, s => '__appSingleton' in s, { parent: this.singletonLocator });
+    return new App(singletonLocator);
   }
 }
 
@@ -82,19 +91,48 @@ export class BaseService {
 
 export class Locator<Context> {
   private instances: Map<Function, any> = new Map();
+  private parent: Locator<Context> | null = null;
+  private overrides: Map<Function, Function> = new Map();
 
   constructor(
     private arg: Context,
     private isClass: (v: ConstructorOrFactory<Context, any>) => boolean,
-    private overrides: Map<Function, Function> = new Map(),
-  ) {}
+    options?: {
+      overrides?: Map<Function, Function>;
+      parent?: Locator<Context>;
+    },
+  ) {
+    options = options || {};
+    this.parent = options.parent || this.parent;
+    this.overrides = options.overrides || this.overrides;
+  }
 
   private isClassTG<T>(v: ConstructorOrFactory<Context, T>): v is ClassConstructor<Context, T> {
     return this.isClass(v);
   }
+
   private instantiate<T>(f: ConstructorOrFactory<Context, T>) {
     if (this.isClassTG(f)) return new f(this.arg);
     else return f(this.arg);
+  }
+
+  private existOnParent<T>(f: ConstructorOrFactory<Context, T>): boolean {
+    if (this.parent) {
+      return this.parent.instances.has(f) || this.parent.existOnParent(f);
+    }
+    return false;
+  }
+
+  private getFromParent<T>(f: ConstructorOrFactory<Context, T>): T {
+    if (this.parent) {
+      if (this.parent.instances.has(f)) {
+        return this.parent.instances.get(f);
+      } else {
+        return this.parent.getFromParent(f);
+      }
+    }
+    // we are at the root, but no component is found
+    throw Error('There is no parent locator to look into.');
   }
 
   get<T>(f: ConstructorOrFactory<Context, T>): T {
@@ -102,6 +140,9 @@ export class Locator<Context> {
       f = this.overrides.get(f) as any;
     }
     if (!this.instances.has(f)) {
+      if (this.existOnParent(f)) {
+        return this.getFromParent(f);
+      }
       this.instances.set(f, this.instantiate(f));
     }
     return this.instances.get(f) as T;
@@ -119,7 +160,7 @@ export class Locator<Context> {
   }
 
   withNewContext(ctx: Context) {
-    return new Locator(ctx, this.isClass, this.overrides);
+    return new Locator(ctx, this.isClass, { overrides: this.overrides });
   }
 }
 
