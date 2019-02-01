ed52fa166ed8c160fdc2b330cfb47c3c5cf0c6e9
Andrej T <andrejt@hfour.com>
2019-02-01 16:39:46 +0100

    backend, database: read DB_MAX_CONNS from env

--- packages/backend/src/db/database.ts
+++ packages/backend/src/db/database.ts
@@ -2,7 +2,10 @@ import { anydbSQL } from 'anydb-sql-2';
 import { AppSingleton, App } from '@h4bff/core';
 
 export class Database extends AppSingleton {
-  db = anydbSQL({ url: process.env['POSTGRES_URL'] });
+  db = anydbSQL({
+    url: process.env['POSTGRES_URL'],
+    connections: { min: 2, max: Number(process.env['DB_MAX_CONNS'] || '20') },
+  });
 
   private migrations: string[] = [];
 
@@ -12,9 +15,5 @@ export class Database extends AppSingleton {
 
   getMigrationsList() {
     return this.migrations;
-  }
-
-  constructor(app: App) {
-    super(app);
   }
 }
