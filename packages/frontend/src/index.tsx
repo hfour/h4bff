945ebf72d5ebc26dcd8d19da7020df3fab00cf4d
Emil Todorovski <emil@hfour.com>
2019-02-05 15:28:02 +0100

    moved to react router 4

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,13 +1,14 @@
 import { AppSingleton } from '@h4bff/core';
 import * as React from 'react';
-import { Router, Route, RouteComponentProps, browserHistory } from 'react-router';
+import { Router, Route, RouteComponentProps } from 'react-router';
+import { browserHistory } from './history'
 
 export type H4Route = {
   path: string;
   component: (rp: RouteComponentProps<any, any>) => JSX.Element;
 };
 
-export type RP = RouteComponentProps<H4Route, any>;
+export type RP = RouteComponentProps<any, any>;
 
 /**
  * Frontend router.
@@ -16,13 +17,14 @@ export class H4Router extends AppSingleton {
   routes: Array<H4Route> = [];
 
   add(route: H4Route) {
+    console.log('add route' , route.component.name)
     this.routes.push(route);
   }
 
   generateRouterJSX() {
     return (
       <Router history={browserHistory}>
-        <Route path="/" component={props => props.children}>
+        <Route path="/" component={(props: any) => props.children}>
           {this.routes.map(route => (
             <Route key={route.path} path={route.path} component={route.component} />
           ))}
