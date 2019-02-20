2cdd2ebe4adb078ee37eeaa07b3d335f01f85e9b
Goran Peshanski <gpeshans@hfour.com>
2019-02-20 10:31:19 +0100

    add docstring for addMiddleware

--- packages/backend/src/rpc/middleware.ts
+++ packages/backend/src/rpc/middleware.ts
@@ -12,6 +12,12 @@ export class RPCMiddlewareContainer extends AppSingleton {
     return Promise.resolve().then(() => dispatcher.handleRequest());
   };
 
+  /**
+   * Adds new middleware to the RPC layer. Make sure that a middleware always returns a result.
+   * It might be a result that the middleware generates or modifies from the previos middleware,
+   * or it could be the result of the "next()" middleware. This way the result will be cascaded
+   * to the end of the middleware chain to achieve consistent success and error handling.
+   */
   addMiddleware(middleware: RPCMiddleware) {
     let oldCall = this.call;
     this.call = (dispatcher: RPCDispatcher) => {
