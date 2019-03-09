b0764453ad7315ad387c8640a9478c94dc0ad7d1
Emil Todorovski <40767477+EmilH4@users.noreply.github.com>
2019-03-09 11:10:36 +0100

    Pluggable router (#12)
    Introduces a pluggable mobx router. 
    
    This should be able to fully substitute the router from react-router, including the <Link> component.
    
    For more information, see the wiki page: https://github.com/hfour/h4bff/wiki/Router-%5Bfrontend%5D
    
    Commits:
    
    * initial implementation of the pluggabe router
    * flat router
    * Add a default route for the main / route.
    * moved to react router 4
    * added mobx router
    * added regex matching
    * sending route params to components
    * added link and navlink
    * fixed navlink not refreshing
    * merged navlink into Link, which doesnt use App or Router now
    * fixed improper handling of multiple classes
    * minor refactor
    * handle query strings in router
    * reversed order of adding routes
    * refactored mainrouter
    * fixed typos and renamed the main router
    * fixed react version to the one resolved in h4-platform
    * fixed bug in Link, removed unnecessary code, refactored MainRouter
    * added tests for the router
    * improved the router tests
    * moved the router components in their own folder. small cleanup. added few tests
    * used mobx to better structure flow in the router. Added simple validation on route adding
    * dont expose MobxRouter; renamed MainRouter to Router; new test about 404; tests cleanup

---
+++ packages/frontend/src/router/router.test.tsx
@@ -0,0 +1,164 @@
+import { Router } from './router';
+import { App } from '@h4bff/core';
+import * as React from 'react';
+import { RouteProvider } from './routeProvider';
+import { Location } from 'history';
+import * as TestRenderer from 'react-test-renderer';
+import * as url from 'url';
+
+const potatoesPage = jest.fn(() => <div>Example page</div>);
+const carsPage = jest.fn(() => <div>Sample page</div>);
+
+describe('router', () => {
+  let app: App;
+  let router: Router;
+
+  const visitUrl = (path: string) => {
+    TestRenderer.create(<router.RenderInstance />);
+
+    const routeProvider = app.getSingleton(RouteProvider);
+    const parsedUrl = url.parse(path);
+
+    routeProvider.location = { pathname: parsedUrl.pathname, search: parsedUrl.search } as Location;
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+    app = new App();
+    router = app.getSingleton(Router);
+  });
+
+  describe('matching routes', () => {
+    it('should match a basic route', () => {
+      router.addRoute('/example', potatoesPage);
+      visitUrl('/example');
+      expect(potatoesPage).toBeCalled();
+    });
+
+    it('should match exact route', () => {
+      router.addRoute('/example', potatoesPage);
+      router.addRoute('/example/route', carsPage);
+      visitUrl('/example/route');
+
+      //checks that the component at the partially matched routes is not rendered.
+      expect(potatoesPage).toBeCalledTimes(0);
+      expect(carsPage).toBeCalled();
+    });
+
+    it('should match only the lastly added route if they have the same path', () => {
+      router.addRoute('/example', potatoesPage);
+      router.addRoute('/example', carsPage);
+      visitUrl('/example');
+
+      expect(potatoesPage).toBeCalledTimes(0);
+      expect(carsPage).toBeCalled();
+    });
+
+    it('should match route ending with wildcard, if path is exact with the route', () => {
+      router.addRoute('/example/*', potatoesPage);
+      visitUrl('/example');
+
+      expect(potatoesPage).toBeCalled();
+    });
+
+    it('should match route ending with wildcard, if path is larger than the route', () => {
+      router.addRoute('/example/*', potatoesPage);
+      visitUrl('/example/route');
+
+      expect(potatoesPage).toBeCalled();
+    });
+  });
+
+  describe('visiting non-existing routes', () => {
+    // todo: do we need more sophisticated 404 logic, like throwing something / printing warning?
+    it('should not render pages when the route doesnt match', () => {
+      router.addRoute('/example', potatoesPage);
+      visitUrl('/example/404');
+      visitUrl('/doesnt-exist');
+
+      expect(potatoesPage).not.toBeCalled();
+    });
+  });
+
+  describe('handling params', () => {
+    it('should extract proper route params', () => {
+      router.addRoute('/example/:paramone/:paramtwo', potatoesPage);
+      visitUrl('/example/1/2');
+
+      let params = potatoesPage.mock.calls[0];
+      expect(params[0].paramone).toEqual('1');
+      expect(params[0].paramtwo).toEqual('2');
+    });
+
+    it('should extract proper query params', () => {
+      router.addRoute('/example', potatoesPage);
+      visitUrl('/example?first=1&second=2');
+
+      let params = potatoesPage.mock.calls[0];
+      expect(params[0].queryParams.first).toEqual('1');
+      expect(params[0].queryParams.second).toEqual('2');
+    });
+
+    it('should extract both query and route params', () => {
+      router.addRoute('/example/:paramone', potatoesPage);
+      visitUrl('/example/p1?first=1&second=2');
+
+      let params = potatoesPage.mock.calls[0];
+      expect(params[0].paramone).toEqual('p1');
+      expect(params[0].queryParams.first).toEqual('1');
+      expect(params[0].queryParams.second).toEqual('2');
+    });
+  });
+
+  describe('handling redirects', () => {
+    it('should perform a simple redirect', () => {
+      router.addRedirect({ from: '/', to: '/example' });
+      visitUrl('/');
+
+      let routeProvider = app.getSingleton(RouteProvider);
+      expect(routeProvider.location.pathname).toEqual('/example');
+    });
+
+    it('should perform a chained (double) redirect', () => {
+      router.addRedirect({ from: '/', to: '/example' });
+      router.addRedirect({ from: '/example', to: '/example/route' });
+      visitUrl('/');
+
+      let routeProvider = app.getSingleton(RouteProvider);
+      expect(routeProvider.location.pathname).toEqual('/example/route');
+    });
+
+    it('should redirect to the lastly added route if both redirects have the same "from"', () => {
+      router.addRedirect({ from: '/', to: '/example' });
+      router.addRedirect({ from: '/', to: '/route' });
+      visitUrl('/');
+
+      let routeProvider = app.getSingleton(RouteProvider);
+      expect(routeProvider.location.pathname).toEqual('/route');
+    });
+
+    it('should redirect from url with query param', () => {
+      router.addRedirect({ from: '/example', to: '/redirected' });
+      visitUrl('/example?query=1');
+
+      let routeProvider = app.getSingleton(RouteProvider);
+      expect(routeProvider.location.pathname).toEqual('/redirected');
+    });
+
+    it('should redirect from url with route param', () => {
+      router.addRedirect({ from: '/example/:param', to: '/redirected' });
+      visitUrl('/example/1');
+
+      let routeProvider = app.getSingleton(RouteProvider);
+      expect(routeProvider.location.pathname).toEqual('/redirected');
+    });
+
+    it('should NOT redirect from url if route param doesnt match', () => {
+      router.addRedirect({ from: '/example/:param', to: '/redirected' });
+      visitUrl('/example');
+
+      let routeProvider = app.getSingleton(RouteProvider);
+      expect(routeProvider.location.pathname).toEqual('/example');
+    });
+  });
+});
